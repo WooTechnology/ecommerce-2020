8ddce0f7c54f11bfd58469033df2826bddcd77c3
Riya Rakshit <46257735+RakshitRiya@users.noreply.github.com>
2020-12-15 19:32:59 +0530

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+
+const mongoose = require("mongoose")
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+
+//My Routes
+const authRoutes = require("./routes/auth")
+const userRoutes = require("./routes/user")
+const categoryRoutes = require("./routes/category")
+const productRoutes = require("./routes/product")
+const orderRoutes = require("./routes/order")
+
+
+//DB Connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("!!--------DB CONNECTED!--------!!");    
+});
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes middlewares
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+if(process.env.ENVIRONMENT === "PRODUCTION"){
+    app.get("/*", (req,res) => console.log(req.connection.remoteAddress + " => " + req.originalUrl));
+}
+
+//PORT
+const port = process.env.PORT || 8000;
+
+//Creating a server
+app.listen(port, () => {
+    console.log(`>> App is running at ${port}`);    
+});
